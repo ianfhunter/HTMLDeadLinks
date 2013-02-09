3fa502302d4e27bc18252fb0f5c6d09df6695f37
Ian Hunter <ianfhunter@gmail.com>
2013-02-09 00:12:37 +0000

    Release version. ignoring write errors, as not reflective of a null link.

--- html.cpp
+++ html.cpp
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <iostream>
 #include <fstream>
+#include <curl/curl.h>
 using namespace std;
 
 struct Node{
@@ -13,6 +14,34 @@ struct Node{
     string * address;
 };
 struct Node * setinel_adr;
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp){}
+
+int open_address(string url){
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);  
+    
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    
+   // if(res != CURLE_OK)
+   //   fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+  }
+ if(res != 0 && res != 23){
+
+	return 1;
+ }
+   return 0;
+}
 
 struct Node * directory_scan(string name, DIR* dir,struct dirent *ent, struct Node *node){
     if ((dir = opendir (name.c_str())) != NULL) {
@@ -54,7 +83,7 @@ struct Node * directory_scan(string name, DIR* dir,struct dirent *ent, struct No
 }
 void check_file_for_duds(string filename){
 	ifstream inFile;
-	cerr  << "\e[0;31m" << filename<< "\e[0;3m" << endl;
+	//cerr  << "\e[0;31mfile: " << filename<< "\e[0m \e[0;31m \e[0;22m \e[0m" << endl;
 	inFile.open(&filename[0], ios::in);
 	if(!inFile){
 		cerr << "error reading file" << endl;
@@ -66,21 +95,47 @@ void check_file_for_duds(string filename){
 		if(pos != std::string::npos){
 			part = part.substr(pos+6,part.rfind("\"") -6);
 			//part is our current html files
-			cerr << part << endl;
+			if(part.find("http") != string::npos){
+			//External
+				//cerr << "\e[0m \e[0;32m " << "link: " << part << "\e[0m"  << endl;
+				if(open_address(part) == 1){
+	                                 cerr <<  "\e[0m \e[0;34m " << "External Link Not Found: " + part <<  "\e[0m" << endl;
+        	                }
+
+			}else{ 
+			//Internal
+				//have to get right position in the folder.
+				//cerr << "\e[0m \e[0;36m " <<"link: " << filename <<"~~"<< part << "\e[0m"  << endl;
+				string cmd = "cd /" + filename.substr(pos,filename.rfind("/"));
+				string wat;
+				system(cmd.c_str());
+				//cmd = "if ls " + filename +" >/dev/null 2>&1 ;then echo FOUND; else echo NONE FOUND; fi";
+				//wat = system(cmd.c_str());
+				//cerr << wat;
+				ifstream ifile(filename.c_str());
+				if(!ifile) cerr << "Internal File Not Found" << endl;
+			}
 		}
 	}
+	cout << "\e[0;30m";
 }
 
 int main(int argc, char* argv[])
 {
+    cerr << "Scanning Directory for webfiles (.php and .html) ..." << endl;
     DIR *dir;
     struct dirent *ent;
     struct Node * addresses = (struct Node*)malloc(sizeof(struct Node));
     struct Node * tmp;
+    //temporary directory plz
     directory_scan("/srv/webspace/ian",dir,ent,addresses);
+    cerr << "done." << endl << endl;
+    cerr << "The next stage is the most time intensive" << endl;
+    cerr << "searching through files for broken links... " << endl;
     for(tmp = addresses; tmp->next != setinel_adr; tmp = tmp->next){ 
         check_file_for_duds(*(tmp->address));
     }
+    cerr << "done. \e[0m" << endl;
     return 0;    
 }
 
